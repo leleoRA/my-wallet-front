ea18c9f7364e65f6791fdaff2574c4338d4eb3ca
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-21 22:31:53 -0300

    Add home/newearning/newexpenditure to router

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import UserContext from './contexts/UserContext';
 import GlobalStyles from './components/GlobalStyles';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Home from './components/Home';
+import NewEarning from './components/NewEarning';
+import NewExpenditure from './components/NewExpenditure';
 
 function App() {
   return (
-    <UserContext.Provider value={{ banana:"1" }}>
+    <UserContext.Provider value={{ userFirstName:"Banana" }}>
       <BrowserRouter>
         <GlobalStyles />
         <Switch>
-          <Route path="/login" component={Login}/> 
-          <Route path="/signup" component={SignUp}/> 
+          <Route exact path="/newexpenditure" component={NewExpenditure}/> 
+          <Route exact path="/newearning" component={NewEarning}/> 
+          <Route exact path="/login" component={Login}/> 
+          <Route exact path="/signup" component={SignUp}/> 
+          <Route exact path="/" component={Home}/> 
         </Switch>
       </BrowserRouter>
     </UserContext.Provider>
