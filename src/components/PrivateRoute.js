afd229aa1cd092673f824a93385ba65b546eb3b6
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-23 12:22:52 -0300

    Add persistent login

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,23 @@
 import {Route, Redirect} from 'react-router-dom';
 
-export default function PrivateRoute ({component: Component, authed, ...rest}) {
+export function PrivateRoute ({component: Component, authed, ...rest}) {
   return (
     <Route
       {...rest}
       render={(props) => authed === true
         ? <Component {...props} />
         : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+export function PublicOnlyRouth ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/', state: {from: props.location}}} />}
     />
   )
 }
