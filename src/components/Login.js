6708e2c72f0433bb01bf3cfb9b05526d373f9c23
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-23 13:55:08 -0300

    Fetch logs upon login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,50 @@
-import styled from 'styled-components';
-import Form from './Form';
-import { useContext, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import axios from 'axios';
-import UserContext from '../contexts/UserContext';
+import styled from "styled-components";
+import Form from "./Form";
+import { useContext, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import UserContext from "../contexts/UserContext";
+import Config from "../helper_functions/Config";
 
-class EmptyForm{
-  constructor(){
+class EmptyForm {
+  constructor() {
     this.email = "";
     this.password = "";
   }
 }
 
-export default function Login(){
+export default function Login() {
   const [formState, setFormState] = useState(new EmptyForm());
   const history = useHistory();
-  const {setUser} = useContext(UserContext);
+  const { setUser, setLogs } = useContext(UserContext);
   function customSubmit() {
     axios
       .post("http://localhost:4000/login", formState)
-      .then(({data}) => {
-        setUser(data);
-        localStorage.setItem("user", JSON.stringify(data))
-        history.push("/")
+      .then(({ data: user }) => {
+        setUser(user);
+        localStorage.setItem("user", JSON.stringify(user));
+        const config = new Config(user.token);
+        return axios.get("http://localhost:4000/logs", config);
+      })
+      .then(({ data: logs }) => {
+        setLogs(logs);
+        history.push("/");
       })
       .catch((e) => alert(e));
   }
-
 
   return (
     <PageWrapper>
       <Logo>MyWallet</Logo>
       <Form customSubmit={customSubmit}>
-        <input 
+        <input
           required
           type="email"
           placeholder="E-mail"
           value={formState.email}
-          onChange={(e)=>{
+          onChange={(e) => {
             formState.email = e.target.value;
-            setFormState({...formState});
+            setFormState({ ...formState });
           }}
         />
         <input
@@ -47,14 +52,16 @@ export default function Login(){
           type="password"
           placeholder="Senha"
           value={formState.password}
-          onChange={(e)=>{
+          onChange={(e) => {
             formState.password = e.target.value;
-            setFormState({...formState});
+            setFormState({ ...formState });
           }}
         />
         <button>Entrar</button>
       </Form>
-      <Footer><Link to="/signup">Primeira vez? Cadastre-se!</Link></Footer>
+      <Footer>
+        <Link to="/signup">Primeira vez? Cadastre-se!</Link>
+      </Footer>
     </PageWrapper>
   );
 }
@@ -68,18 +75,18 @@ const PageWrapper = styled.div`
   min-height: 100vh;
   width: 100%;
 
-  h1{
+  h1 {
     margin-bottom: 24px;
   }
 
-  form{
+  form {
     margin-bottom: 36px;
   }
-`
+`;
 const Logo = styled.h1`
   font-size: 32px;
   line-height: 50px;
-  font-family: 'Saira Stencil One', cursive;
+  font-family: "Saira Stencil One", cursive;
 `;
 
 const Footer = styled.footer`
@@ -87,6 +94,3 @@ const Footer = styled.footer`
   font-size: 15px;
   line-height: 18px;
 `;
-
-
-
