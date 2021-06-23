71e2a4a48da6578f5a0ba1bbd585697290fcb26a
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-23 12:24:51 -0300

    Implement persistent login routes

--- src/App.js
+++ src/App.js
@@ -8,24 +8,22 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import NewLog from "./components/NewLog";
-import PrivateRoute from "./components/PrivateRoute";
+import {PrivateRoute, PublicOnlyRoute} from "./components/PrivateRoute";
 
 function App() {
   const [user, setUser] = useState(null);
   const [isFirstRender, setIsFirstRender] = useState(true);
-  const location = useLocation();
-  const history = useHistory();
-  
+
   useEffect(() => {
     setIsFirstRender(false);
     const userStorage = localStorage.getItem("user");
-    if (userStorage !== null) setUser(JSON.parse(userStorage));
-    if (userStorage !== null && location.pathname === "/signup") {
-      history.push("/");
+    if (userStorage !== null) {
+      setUser(JSON.parse(userStorage));
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const authed = user === null ? false : true;
+  
   return isFirstRender ? (
     <></>
   ) : (
@@ -33,19 +31,13 @@ function App() {
       <BrowserRouter>
         <GlobalStyles />
         <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
+          <PublicOnlyRoute exact path="/login" component={Login} />
+          <PublicOnlyRoute exact path="/signup" component={SignUp} />
           <PrivateRoute
             authed={authed}
             exact
-            path="/new/earning"
-            component={() => NewLog("earning")}
-          />
-          <PrivateRoute
-            authed={authed}
-            exact
-            path="/new/expenditure"
-            component={() => NewLog("expenditure")}
+            path="/new/:logType"
+            component={NewLog}
           />
           <PrivateRoute authed={authed} exact path="/" component={Home} />
         </Switch>
